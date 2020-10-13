e8eadcd10646cb52876da351606f79a801ce61a8
Adam Robillard <hello@adamrobillard.ca>
2020-10-13 18:26:08 -0400

    first commit

---
+++ src/build/precache-manifest.1fe875d9a19b5e18310ef1b4139adc89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d5c528a35b02eb1704947007d546f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b71a27e4c22dd6a359",
+    "url": "/static/css/main.ddfa4722.chunk.css"
+  },
+  {
+    "revision": "9a424338ecad9cea0105",
+    "url": "/static/js/2.c40573eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c40573eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b71a27e4c22dd6a359",
+    "url": "/static/js/main.6ef024e3.chunk.js"
+  },
+  {
+    "revision": "770344394e9f4453d0a9",
+    "url": "/static/js/runtime-main.3a907230.js"
+  }
+]);
